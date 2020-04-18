b4fac2d44b0d5700e93d0b9f003de857ac10604d
Caio Rodrigues <rfcaio90@gmail.com>
2020-04-17 22:26:32 -0300

    improve recipe admin page

--- recipe/admin.py
+++ recipe/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Recipe
 
+class RecipeList(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'created_at')
+    list_display_links = ('id', 'name')
+    list_filter = ('category',)
+    list_per_page = 5
+    search_fields = ('name',)
 
-admin.site.register(Recipe)
+admin.site.register(Recipe, RecipeList)
