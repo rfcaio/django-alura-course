1f5526bc16ff579a94bea47f51aa688018319848
Caio Rodrigues <rfcaio90@gmail.com>
2020-04-21 21:37:28 -0300

    improve recipe administration page

--- recipe/admin.py
+++ recipe/admin.py
@@ -4,8 +4,9 @@ from .models import Recipe
 
 
 class RecipeList(admin.ModelAdmin):
-    list_display = ('id', 'name', 'category', 'created_at')
+    list_display = ('id', 'name', 'category', 'created_at', 'is_published')
     list_display_links = ('id', 'name')
+    list_editable = ('is_published',)
     list_filter = ('category',)
     list_per_page = 5
     search_fields = ('name',)
