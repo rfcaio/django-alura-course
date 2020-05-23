661bb749a31df3ce0c7c36a94c4721df17e11e3b
Caio Rodrigues <rfcaio90@gmail.com>
2020-05-23 17:44:31 -0300

    add sign in logic

--- user/views.py
+++ user/views.py
@@ -1,25 +1,46 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
+
 def signin(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if email == '' or password == '':
+            print('A valid email and password are required.')
+            return redirect('sigin')
+
+        if User.objects.filter(email=email).exists():
+            username = (
+                User.objects
+                    .filter(email=email)
+                    .values_list('username', flat=True)
+                    .get()
+            )
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
     return render(request, 'signin.html')
 
 def signup(request):
-    if (request.method == 'POST'):
+    if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
 
-        if (not username.strip()):
+        if not username.strip():
             print('A valid user name is required.')
             return redirect('signup')
 
-        if (password != confirm_password):
+        if password != confirm_password:
             print('The two passwords are not equals.')
             return redirect('signup')
 
-        if (User.objects.filter(email=email).exists()):
+        if User.objects.filter(email=email).exists():
             print('A user with email %s already exists.' % email)
             return redirect('signup')
 
